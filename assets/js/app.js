8599265f55995c2fec5d875bae470af71b33d4a9
Konstantin <Steelmen87@bk.ru>
2022-01-26 16:12:09 +0300

    scrollSpy ready

--- assets/js/app.js
+++ assets/js/app.js
@@ -32,8 +32,57 @@ $(function () {
         let scrollELPosition = $(scrollEL).offset().top;
 
         $("html,body").animate({
-            scrollTop: scrollELPosition-headerH
+            scrollTop: scrollELPosition - headerH
         }, 500)
     })
+    /*ScrollSpy */
+    let windowH = $(window).height();
+
+    $(window).on("scroll", function () {
+
+        let scrollTop = $(this).scrollTop();
+
+        $("[data-scrollspy]").each(function () {
+
+            let $this = $(this);
+            let sectionId = $(this).data('scrollspy');
+
+            let sectionOffset = $this.offset().top;
+            sectionOffset -= windowH * 0.3;
+
+            if (scrollTop >= sectionOffset) {
+                $('#nav [data-scroll]').removeClass('active');
+                $('#nav [data-scroll="' + sectionId + '"]').addClass('active');
+            }
+            if (scrollTop <= 0) {
+                $('#nav [data-scroll]').removeClass('active');
+            }
+        })
+
+    });
 
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
